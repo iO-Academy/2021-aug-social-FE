5f1b21b48a4f1017223c3b326da6fb1680369d60
DevUsman-18 <mohammad_usman117@hotmail.co.uk>
2021-12-14 13:38:12 +0000

    landing page created in react

---
+++ social-app-fe/src/AppTitle/AppTitle.js
@@ -0,0 +1,6 @@
+
+function AppTitle() {
+    return <h1>Cool app title</h1>
+}
+
+export default AppTitle;
---
+++ social-app-fe/src/LoginButton/LoginButton.js
@@ -0,0 +1,22 @@
+// import { createMuiTheme, ThemeProvider } from '@material-ui/core';
+import Button from '@mui/material/Button';
+
+// const theme = createMuiTheme({
+//     palette: {
+//         primary: {
+//             main: "#7400B8"
+//         },
+//         secondary: {
+//             main: "#ffa500"
+//         }
+//     }
+// })
+
+
+function LoginButton() {
+    return (
+        <Button variant="contained" color="purpley">Login</Button>
+    )
+}
+
+export default LoginButton;
